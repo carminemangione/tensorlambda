bfd21930f5a3d05852b940c822d3d234082923f0
carmine <carmine@mangione.com>
2018-04-23 19:33:28 -0700

    Configuring clojure with appropriate build targets.

--- src/test/clojure/com/mangione/continuous/first_model_test.clj
+++ src/test/clojure/com/mangione/continuous/first_model_test.clj
@@ -1,6 +1,12 @@
-(ns com.mangione.continuous.first-model-test
-  (:require [clojure.test :refer :all])
-  (use clojure.core$junit)
+(ns com.mangione.continuous.first_model_test
+  (:require [com.mangione.continuous.first_model :as first_model]
+              [clojure.test :refer :all]))
 
-  (deftest addition
-           (is (= 4 (+2 2)))))
+(deftest addition
+  (is (= 4 (+ 2 2)))
+  (is (= 7 (+ 3 4))))
+
+(deftest codger_test
+  (is (=  ["Get off my lawn, Sam" (first_model/codger "Sam")] )))
+
+(run-tests)
