cc77eaf340d7f77e48e3232c35f24f6a5d16f999
carmine.mangione <carmine.mangione@servicenow.com>
2018-04-25 16:45:10 -0700

    beginning of variable bindings

--- src/main/clojure/com/mangione/continuous/variables/bound_variable.clj
+++ src/main/clojure/com/mangione/continuous/variables/bound_variable.clj
@@ -1,8 +1,15 @@
-(ns com.mangione.continuous.variables.bound_variable)
+(ns com.mangione.continuous.variables.bound_variable
+  (:import (java.util.function Function)))
 
 (defn create [name, column, transformation]
-  [name, column, transformation])
+  [name, column, (apply transformation [1])])
 
 (defn name [bound] (get bound 0))
 (defn column [bound] (get bound 1))
 (defn calc [bound, x] (apply (get bound 2) [x]))
+
+(defn my-function[]
+  (reify
+    Function
+    (apply [this arg]
+      (inc arg))))
--- src/test/clojure/com/mangione/continuous/variables/variables_test.clj
+++ src/test/clojure/com/mangione/continuous/variables/variables_test.clj
@@ -1,16 +1,19 @@
 (ns com.mangione.continuous.variables.variables-test
   (:require [clojure.test :refer :all])
-  (:require [com.mangione.continuous.variables.bound_variable :as bound_variable :refer [create]]))
+  (:require [com.mangione.continuous.variables.bound_variable :as bound_variable :refer [create]])
+  (:import (com.mangione.continuous.variables BoundVariable)))
 
 (defn myfunc [var] (inc var))
 (deftest bound_variable-create
   (print (create "var_name" 2 myfunc))
-  (is ["var_name" 2 myfunc] (create "var_name" 2 myfunc)))
+  (is ["var_name" 2 myfunc] (create "var_name" 2 (resolve 'myfunc))))
 
 (deftest name-test
   (is "var_name" (bound_variable/name (create "var_name" 2 myfunc))))
 
 (deftest apply-test
   (is 4 (bound_variable/calc (create "var_name" 2 myfunc) 3)))
+
+(doto (BoundVariable.) (.execute (bound_variable/my-function)))
 
 (run-tests)
