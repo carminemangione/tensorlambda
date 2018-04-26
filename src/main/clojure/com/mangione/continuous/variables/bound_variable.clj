dd7f3d3389349e0e309ce73917122f3853757ecb
carmine <carmine@mangione.com>
2018-04-25 20:21:59 -0700

    Configuring clojure with appropriate build targets.

--- src/main/clojure/com/mangione/continuous/variables/bound_variable.clj
+++ src/main/clojure/com/mangione/continuous/variables/bound_variable.clj
@@ -8,8 +8,19 @@
 (defn column [bound] (get bound 1))
 (defn calc [bound, x] (apply (get bound 2) [x]))
 
-(defn my-function[]
+(defn my-function []
   (reify
     Function
     (apply [this arg]
       (inc arg))))
+
+
+(defn ^Function as-function [f]
+  '(reify Function
+    (apply [arg] (f arg))))
+
+(defmacro jfn [& args]
+  `(as-function (fn ~@args)))
+
+
+
