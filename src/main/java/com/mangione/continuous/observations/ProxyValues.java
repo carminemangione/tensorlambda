855faacf25d52674ae48f6e78dbf21230efe6429
raman.kathuria <raman.kathuria@servicenow.com>
2018-11-06 15:31:32 -0800

    Added synchronization for proxyValues

--- src/main/java/com/mangione/continuous/observations/ProxyValues.java
+++ src/main/java/com/mangione/continuous/observations/ProxyValues.java
@@ -7,39 +7,39 @@ public class ProxyValues {
 
 	private BiMap<String, Integer> biMap = HashBiMap.create();
 
-	boolean contains(Integer i) {
+	synchronized boolean contains(Integer i) {
 		return biMap.inverse().containsKey(i);
 	}
 
-	boolean contains(String i) {
+	synchronized boolean contains(String i) {
 		return biMap.containsKey(i);
 	}
 
-	public void addPair(Integer i, String val) {
+	synchronized public void addPair(Integer i, String val) {
 		biMap.inverse().put(i, val);
 	}
 
-	public void addPair(String i, Integer val) {
+	synchronized public void addPair(String i, Integer val) {
 		biMap.put(i, val);
 	}
 
-	public void add(String i) {
+	synchronized public void add(String i) {
 		if(!contains(i))
 			biMap.put(i, biMap.size());
 	}
 
-	public String getName(Integer i) {
+	synchronized public String getName(Integer i) {
 		return biMap.inverse().get(i);
 	}
 
-	public Integer getName(String i) { return biMap.get(i); }
+	synchronized public Integer getName(String i) { return biMap.get(i); }
 
 	@Override
-	public String toString(){
+	synchronized public String toString(){
 		return biMap.toString();
 	}
 
-	public int size() {
+	synchronized public int size() {
 		return biMap.size();
 	}
 }
