7aff695b9bf7f9601237b0c1bd66e6207b6c289a
carmine.mangione <carmine.mangione@servicenow.com>
2018-11-16 13:32:43 -0800

    Added resume ability.

--- src/main/java/com/mangione/continuous/datagenerators/RandomUnitVector.java
+++ src/main/java/com/mangione/continuous/datagenerators/RandomUnitVector.java
@@ -4,27 +4,16 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.random.RandomGenerator;
 
 public class RandomUnitVector {
-    private final int numberOfDimensions;
-    private final RandomGenerator randomGenerator;
+
     private final RandomGaussianVector randomGaussianVector;
 
     public RandomUnitVector(int numberOfDimensions, RandomGenerator randomGenerator) {
-        this.numberOfDimensions = numberOfDimensions;
-        this.randomGenerator = randomGenerator;
-        this.randomGaussianVector = new RandomGaussianVector(numberOfDimensions,0.0,1.0,false,randomGenerator);
+        this.randomGaussianVector = new RandomGaussianVector(numberOfDimensions,0.0,1.0,false,
+                randomGenerator);
     }
 
     public double[] nextVector() {
         double[] vector = this.randomGaussianVector.nextVector();
         return new ArrayRealVector(vector).unitVector().toArray();
-    }
-
-    // This is wrong. It's not UNIFORMLY distributed on the sphere.
-    private double[] createAndFillVector() {
-        double[] vector = new double[numberOfDimensions];
-        for (int i = 0; i < vector.length; i++) {
-            vector[i] = 1.0 - 2.0 * randomGenerator.nextDouble();
-        }
-        return vector;
     }
 }
