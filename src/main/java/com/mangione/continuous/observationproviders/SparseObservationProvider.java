4a7959db2d18f32c999c01a9e9f31eee10a13b37
raman.kathuria <raman.kathuria@servicenow.com>
2019-01-23 11:08:07 -0800

    Updated KMODES

---
+++ src/main/java/com/mangione/continuous/observationproviders/SparseObservationProvider.java
@@ -0,0 +1,37 @@
+package com.mangione.continuous.observationproviders;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import com.mangione.continuous.model.modelproviders.DataProvider;
+
+public class SparseObservationProvider implements DataProvider<HashSet<Integer>> {
+
+	private final ArrayList<HashSet<Integer>> sparseObservations;
+	private final int lengthOfObs;
+
+	public SparseObservationProvider(ArrayList<HashSet<Integer>> data, int lengthOfObs) {
+		this.sparseObservations = data;
+		this.lengthOfObs = lengthOfObs;
+	}
+
+	@Override
+	public int getNumberOfLines() {
+		return this.sparseObservations.size();
+	}
+
+	public HashSet<Integer> get(int index) {
+		try {
+			return sparseObservations.get(index);
+		} catch (Throwable e) {
+			System.out.println("Index: " + index);
+			return null;
+		}
+	}
+
+	@Override
+	public int getLengthOfObservation() {
+		return this.lengthOfObs;
+	}
+
+}
