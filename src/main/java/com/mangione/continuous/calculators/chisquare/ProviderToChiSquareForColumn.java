12347a57458a4e838f819549fc65e827b70e7aa5
carmine.mangione <carmine.mangione@servicenow.com>
2019-02-21 16:27:09 -0800

    Added SparseObservations and SparseExemplars and refactored shared behavior

--- src/main/java/com/mangione/continuous/calculators/chisquare/ProviderToChiSquareForColumn.java
+++ src/main/java/com/mangione/continuous/calculators/chisquare/ProviderToChiSquareForColumn.java
@@ -1,16 +1,16 @@
 package com.mangione.continuous.calculators.chisquare;
 
 import com.mangione.continuous.observationproviders.ObservationProvider;
-import com.mangione.continuous.observations.sparse.SparseExemplarInterface;
+import com.mangione.continuous.observations.ExemplarInterface;
 
 @SuppressWarnings("WeakerAccess")
 public class ProviderToChiSquareForColumn {
 	private final ChiSquare fChiSquare;
 
-	public ProviderToChiSquareForColumn(ObservationProvider<Integer, ? extends SparseExemplarInterface<Integer, Integer>> provider, int column) {
+	public ProviderToChiSquareForColumn(ObservationProvider<Integer, ? extends ExemplarInterface<Integer, Integer>> provider, int column) {
 
 		ContingencyTable.Builder builder = new ContingencyTable.Builder(3, 3);
-		for (SparseExemplarInterface<Integer, Integer> next : provider)
+		for (ExemplarInterface<Integer, Integer> next : provider)
 			builder.addObservation(next.getFeature(column), next.getTarget());
 
 		fChiSquare = new ChiSquare(builder.build());
