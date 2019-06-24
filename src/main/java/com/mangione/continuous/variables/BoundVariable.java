2b03771f689f1f6fe182ef3df2d6c392c1224f67
carmine.mangione <carmine.mangione@servicenow.com>
2019-06-24 13:52:41 -0700

    Revert "Added sparse rows"
    This reverts commit 710445ca

--- src/main/java/com/mangione/continuous/variables/BoundVariable.java
+++ src/main/java/com/mangione/continuous/variables/BoundVariable.java
@@ -3,9 +3,10 @@ package com.mangione.continuous.variables;
 import java.util.function.Function;
 
 public class BoundVariable {
-
 	public Object execute(Function<? super Object, ? super Object> function) {
-		return function.apply(new Integer(1));
+		Object apply = function.apply(1);
+		System.out.println("apply = " + apply);
+		return apply;
 	}
 
 }
