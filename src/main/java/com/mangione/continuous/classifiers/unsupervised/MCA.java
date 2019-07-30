433e6f5499926cb5dfd843d09d4cd0da4f36f479
aditya.yellumahanti <aditya.yellumahanti@servicenow.com>
2019-07-30 11:44:54 -0700

    Converting sparse to dense matrix, changing stuff from simplematrices to dmatrixrmaj's

--- src/main/java/com/mangione/continuous/classifiers/unsupervised/MCA.java
+++ src/main/java/com/mangione/continuous/classifiers/unsupervised/MCA.java
@@ -3,17 +3,23 @@ package com.mangione.continuous.classifiers.unsupervised;
 import com.mangione.continuous.observationproviders.ObservationProviderInterface;
 import com.mangione.continuous.observations.ObservationInterface;
 import com.mangione.continuous.observations.dense.Observation;
+import org.apache.commons.lang3.ObjectUtils;
 import org.ejml.data.DMatrixRMaj;
+import org.ejml.data.DMatrixSparseCSC;
 import org.ejml.dense.row.CommonOps_DDRM;
 import org.ejml.simple.SimpleMatrix;
+import org.ejml.sparse.csc.CommonOps_DSCC;
+import sun.java2d.marlin.DMarlinRenderingEngine;
 
+import javax.annotation.Nullable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MCA<S extends Number, T extends ObservationInterface<S>> {
 
     private ObservationProviderInterface<S, T> provider;
-    private int r;
+    private int r; /* the rank of our SVD */
     private int batchSize;
     private int n;
     private SimpleMatrix U;
@@ -40,36 +46,50 @@ public class MCA<S extends Number, T extends ObservationInterface<S>> {
 
     /*
     ##############################################
-                  Init and Modification
+           Finding out Modification Matrices
     ##############################################
      */
 
-
-    private DMatrixRMaj burt(DMatrixRMaj Z) {
-        DMatrixRMaj Zt = Z.copy();
+    private DMatrixRMaj sparseToDense(DMatrixSparseCSC A) {
+        int cols = A.numCols;
+        double[] ones = new double[cols];
+        int i;
+        for(i = 0; i < cols; i++) {
+            ones[i] = 1;
+        }
+        DMatrixRMaj I = CommonOps_DDRM.diag(ones);
         DMatrixRMaj C = new DMatrixRMaj();
-        CommonOps_DDRM.transpose(Zt);
-        CommonOps_DDRM.mult(Zt, Z, C);
+        CommonOps_DSCC.mult(A, I, C);
         return C;
     }
 
-    private SimpleMatrix pUpdate(SimpleMatrix C, int Q, int n, int nPlus) {
-        return C.divide((n + nPlus) + Q * Q);
+    private DMatrixRMaj burt(DMatrixSparseCSC Z) {
+        DMatrixSparseCSC lowerC = new DMatrixSparseCSC(Z.numRows, Z.numCols);
+        CommonOps_DSCC.innerProductLower(Z, lowerC, null, null);
+        DMatrixSparseCSC C = new DMatrixSparseCSC(Z.numRows, Z.numCols);
+        CommonOps_DSCC.symmLowerToFull(lowerC, C, null);
+        return sparseToDense(C);
     }
 
-    private ArrayList sUpdate(SimpleMatrix Dr, SimpleMatrix P, SimpleMatrix r, int update){
-        SimpleMatrix DrMod = Dr.elementPower(-1/2);
-        SimpleMatrix mid = P.minus(r.transpose().mult(r));
-        SimpleMatrix A = DrMod.mult(mid);
-        SimpleMatrix S = A.mult(DrMod);
-        ArrayList<SimpleMatrix> sabHolder = new ArrayList<>();
-        sabHolder.add(S);
-        if(update == 1) {
-            sabHolder.add(A);
-            sabHolder.add(DrMod);
-        }
-        return sabHolder;
+    private DMatrixRMaj pUpdate(DMatrixRMaj C, int Q, int n, int nPlus) {
+        double a = (n + nPlus) + Q * Q;
+        CommonOps_DDRM.divide(a, C);
+        return C;
     }
+
+//    private ArrayList sUpdate(DMatrixRMaj Dr, DMatrixRMaj P, DMatrixRMaj r, int update){
+//        DMatrixRMaj DrMod = Dr.elementPower(-1/2);
+//        DMatrixRMaj mid = P.minus(r.transpose().mult(r));
+//        DMatrixRMaj A = DrMod.mult(mid);
+//        DMatrixRMaj S = A.mult(DrMod);
+//        ArrayList<SimpleMatrix> sabHolder = new ArrayList<>();
+//        sabHolder.add(S);
+//        if(update == 1) {
+//            sabHolder.add(A);
+//            sabHolder.add(DrMod);
+//        }
+//        return sabHolder;
+//    }
 
     /*
     ##############################################
