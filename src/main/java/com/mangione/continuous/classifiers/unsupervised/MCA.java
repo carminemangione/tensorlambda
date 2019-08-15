7c1fd8ce05e9af6bd0a8917803ed29b99e767191
aditya.yellumahanti <aditya.yellumahanti@servicenow.com>
2019-08-15 11:48:20 -0700

    beginning on Z creation

--- src/main/java/com/mangione/continuous/classifiers/unsupervised/MCA.java
+++ src/main/java/com/mangione/continuous/classifiers/unsupervised/MCA.java
@@ -2,16 +2,28 @@ package com.mangione.continuous.classifiers.unsupervised;
 
 import com.mangione.continuous.observationproviders.ObservationProviderInterface;
 import com.mangione.continuous.observations.ObservationInterface;
+import com.mangione.continuous.observations.ProxyValuesMultiColumn;
 import com.mangione.continuous.observations.dense.Observation;
 import org.ejml.data.DMatrixRMaj;
+import org.ejml.data.DMatrixSparseCSC;
 
-public class MCA<S extends Number, T extends ObservationInterface<S>> {
-    private ObservationProviderInterface<S, T> provider;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+
+/* implement using provider at some point, make sure to get the file implementation first */
+public class MCA<S extends Number, T extends ObservationInterface<S>>  {
+    private ObservationProviderInterface<S,T> provider;
+    private ProxyValuesMultiColumn pv;
     int rank, batchSize;
-    DMatrixRMaj U, S, V;
-    public MCA(ObservationProviderInterface<S, T> provider, int rank, int batchSize) {
+    DMatrixRMaj U, Sig, V, Dr;
+    public MCA(File file, int rank, int batchSize) throws IOException {
         /* declare U, S, V for first update */
-        /* need to create setter for U, S, V basically so that if we remove columns, we still have
-        *  zeros in place of the removed columns as opposed to literally lopping them off */
+        this.pv = new ProxyValuesMultiColumn(file);
+        /* now proxy to sparse */
+    }
+
+    private void createZ(ProxyValuesMultiColumn pv, ObservationProviderInterface<S, T> provider) throws IOException {
+        DMatrixSparseCSC Z = new DMatrixSparseCSC(this.batchSize, pv.getNumLevels());
     }
 }
